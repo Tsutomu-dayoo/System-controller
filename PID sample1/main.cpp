da894e61a1e28abb07a98ef1cf601ae4c8ba42c4
山口勉 <yamaguchitsutomu@yamaguchitsutomunomacbook-puro.local>
2018-06-26 14:08:18 +0900

    Initial Commit

---
+++ PID sample1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PID sample1
+//
+//  Created by 山口勉 on 2018/06/26.
+//  Copyright © 2018年 山口勉. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
